66c101953994e3047f631c7166472a725c98ba20
Nivetharoyar <117559179+Nivetharoyar@users.noreply.github.com>
2022-11-06 11:28:15 +0530

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Car {
+	//t run as java application we need main method
+	public static void main(String[] args)
+	{
+		//to print syso
+	System.out.println(5+6);	
+	}
+	byte noOfCars= 35;
+	int registerationNumber = 233445567;
+	short carNumber= 234;
+	long number= 4566767L;
+	float carPrice= 3445.456F;
+	double carUniqueNo = 24.4676879;
+	char carName='Z';
+	boolean isItAutomatic = false;
+	
+	
+	
+
+}
